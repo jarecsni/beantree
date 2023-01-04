d93efa02a110540f8d9dde41e8eb4bc818996ad7
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-01-04 09:06:28 +0000

    Some broken tests to fix

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,9 @@
 import { expect, test } from '@playwright/test';
+import { PlaywrightDevPage } from './playwright-dev-page';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+	const playwrightDev = new PlaywrightDevPage(page);
+  	await playwrightDev.goto();
+  	await playwrightDev.getStarted();
+	expect(await page.textContent('a')).toBe('Welcome to SvelteKit');
 });
