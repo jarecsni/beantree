5f385f0878cb3c345fa3039ebfc7e3627b5fd7e5
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-01-24 08:45:38 +0000

    Fix state attribute to be optional

--- src/lib/beans/BeanTree.ts
+++ src/lib/beans/BeanTree.ts
@@ -3,7 +3,7 @@ export type KVType = {[key: string]: unknown}
 export type BeanTreeNode = {
     bean:string,
     instanceId:string,
-    state:{value: unknown},
+    state?:{value: unknown},
     children?:BeanTreeNode[],
     props?:KVType
 }
--- src/routes/examples/three/+page.svelte
+++ src/routes/examples/three/+page.svelte
@@ -22,6 +22,7 @@
 
 <style>
     .content {
-        background: lightgray
+        background: lightgray;
+        display: flex;
     }
 </style>
