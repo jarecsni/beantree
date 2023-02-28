61277cd6988b9797785ad64a18b7b8081051315b
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-02-28 22:28:35 +0000

    Fix TS problem

--- src/lib/beans/jar/slotcontainer/SlotContainer.svelte
+++ src/lib/beans/jar/slotcontainer/SlotContainer.svelte
@@ -3,7 +3,7 @@
 
 <script lang="ts">
 	import GenericBean from '$lib/beans/GenericBean.svelte';
-    import type { BeanTreeNode } from '$lib/beans/BeanTree';
+    import type { BeanTreeNode } from '$lib/beans/tree/BeanTreeNode';
     export let bean1:BeanTreeNode;
     export let bean2:BeanTreeNode;
 </script>
