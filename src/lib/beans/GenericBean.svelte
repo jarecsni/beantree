d8a86413770910c925e59e6ee23fbd0e17245e88
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-02-28 23:44:42 +0000

    Render selected component with border

--- src/lib/beans/GenericBean.svelte
+++ src/lib/beans/GenericBean.svelte
@@ -1,14 +1,19 @@
-
-<svelte:component this={beanRendererComponent} {bean} {...props}/>
+<div class:selected>
+	<svelte:component this={beanRendererComponent} {bean} {...props}/>
+</div>
 
 <script lang="ts">
-	// TODO Svelte does not currently support dynamic named slots, so children will be in an array
-	import GenericBean from '$lib/beans/GenericBean.svelte';
 	import type { SvelteComponent } from 'svelte';
 	import type { BeanTreeNode, KVType } from './tree/BeanTreeNode';
 	import { getPlatformSpecificRenderer } from './utils';
+	import { selectedInstanceId } from './jar/helloworld/store';
+	import { getTreeNodePath } from './tree/BeanTreeNode';
     export let bean:BeanTreeNode;
 	const props:KVType = bean.props || {}
+	let selected = false;
+	$:{
+		selected = getTreeNodePath(bean) ==  $selectedInstanceId;
+	}
 	if (bean.children) {
 		bean.children.forEach(childBean => {
 			childBean.parent = bean;
@@ -18,3 +23,8 @@
 	bean.props = props;
     const beanRendererComponent:typeof SvelteComponent | undefined = getPlatformSpecificRenderer(bean);
 </script>
+<style>
+	.selected {
+		border: 1px dotted;
+	}
+</style>
