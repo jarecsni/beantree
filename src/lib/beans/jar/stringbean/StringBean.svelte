20ee42c548e827cb0ce570844a7f0fee0b97c284
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-04-24 17:37:25 +0100

    Add missing export

--- src/lib/beans/jar/stringbean/StringBean.svelte
+++ src/lib/beans/jar/stringbean/StringBean.svelte
@@ -1,5 +1,7 @@
 <span>{text}</span>
 <script lang="ts">
+	import type { BeanTreeNode } from '$lib/beans/tree/BeanTreeNode';
+
     export let text:string;
     export let bean:BeanTreeNode;
 </script>
