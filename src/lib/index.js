1c7d4c3ea2034b7d0f0cff1614f371ceeefed66b
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-05-23 07:51:32 +0100

    beantree 0.1.1 test in testapp

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+import GenericBean from './beans/GenericBean.svelte';
+export {
+    GenericBean
+}
--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -2,5 +2,5 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <script lang="ts">
-
+	import {GenericBean} from 'beantree';
 </script>
