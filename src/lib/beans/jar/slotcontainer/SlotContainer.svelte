e1e1ce736c2bb135994211f656566e2065839f9e
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2023-06-01 20:00:44 +0100

    Remove unused export

--- src/lib/beans/jar/slotcontainer/SlotContainer.svelte
+++ src/lib/beans/jar/slotcontainer/SlotContainer.svelte
@@ -6,5 +6,4 @@
     import type { BeanTreeNode } from '$lib/beans/tree/BeanTreeNode';
     export let bean1:BeanTreeNode;
     export let bean2:BeanTreeNode;
-    export let bean:BeanTreeNode;
 </script>
