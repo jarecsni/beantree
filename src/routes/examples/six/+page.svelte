6eaa94f0366264f5850ecb6da83b796631606e52
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-08-02 07:53:18 +0100

    Minor fixes

--- src/routes/examples/six/+page.svelte
+++ src/routes/examples/six/+page.svelte
@@ -1,17 +1,18 @@
-Six
+<App/>
 
 <script lang="ts">
-    import { Streamer } from "./datastream/Streamer";
-    Streamer.getInstance().initialise();
-    Streamer.getInstance().connect('ABC', (symbol:string, number:number) => {
-        console.log(symbol, number);
-    });
-    Streamer.getInstance().connect('EFG', (symbol:string, number:number) => {
-        console.log(symbol, number);
-    });
-    Streamer.getInstance().startStreaming();
-    setTimeout(() => {
-        console.log('stopping stream');
-        Streamer.getInstance().stopStreaming();
-    }, 10000);
+    import App from './App.svelte';
+    // import { Streamer } from "./datastream/Streamer";
+    // Streamer.getInstance().initialise();
+    // Streamer.getInstance().connect('ABC', (symbol:string, number:number) => {
+    //     console.log(symbol, number);
+    // });
+    // Streamer.getInstance().connect('EFG', (symbol:string, number:number) => {
+    //     console.log(symbol, number);
+    // });
+    // Streamer.getInstance().startStreaming();
+    // setTimeout(() => {
+    //     console.log('stopping stream');
+    //     Streamer.getInstance().stopStreaming();
+    // }, 10000);
 </script>
