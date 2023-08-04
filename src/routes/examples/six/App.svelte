cb4ecf5eb632fc685096b52bd9df8be50519ff4a
Janos Jarecsni <janos.jarecsni@gmail.com>
2023-08-04 08:07:38 +0100

    Add counterparty picker component

--- src/routes/examples/six/App.svelte
+++ src/routes/examples/six/App.svelte
@@ -1,3 +1,36 @@
-<div>
-    App
+<div class="app">
+    <div class="toolbar">
+        <CounterpartyPicker {counterparties} selectedCounterparty={counterparties[0]}/>
+    </div>
+    <div class="container">
+        
+    </div>
+    <div class="bottom-bar">Bottom Bar</div>
 </div>
+
+<script lang="ts">
+    import CounterpartyPicker from "./components/counterparty/CounterpartyPicker.svelte";
+	import { CounterpartyService } from "./components/counterparty/CounterpartyService";
+    const counterparties = CounterpartyService.getInstance().getCounterparties();
+</script>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+    .toolbar {
+        display: flex;
+    }
+    .container {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .bottom-bar {
+        display: flex;
+    }
+    .ticker {
+        width: 200px;
+    }
+</style>
---
+++ src/routes/examples/six/components/counterparty/CounterpartyPicker.svelte
@@ -0,0 +1,14 @@
+<Autocomplete
+    options={counterparties}
+    bind:value={selectedCounterparty}
+    label="Counterparty"
+    getOptionLabel={(option) =>
+        option ? `${option.label} (${option.id})` : ''}
+/>
+
+<script lang="ts">
+    import Autocomplete from '@smui-extra/autocomplete';
+	import type { Counterparty } from './CounterpartyService';
+    export let selectedCounterparty:Counterparty;
+    export let counterparties:Counterparty[] = [];
+</script>
